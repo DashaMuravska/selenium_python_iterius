2879c2a51032abfba714a93c9c64c3c47d0729e2
DashaMuravska <muravska.daria7@gmail.com>
2022-12-20 22:11:27 +0200

    login

---
+++ chromedriver/login.py
@@ -0,0 +1,41 @@
+import driver as driver
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+import time
+import auth_data
+
+url = "https://iterius.smissltd.com/home"
+driver = webdriver.Chrome(executable_path='/Users/daramuravskaa/PycharmProjects/selenium/chromedriver/chromedriver')
+
+
+try:
+    driver.get(url=url)
+    time.sleep(2)
+    close_button = driver.find_element(By.CLASS_NAME, '_closeBtn_70ilu_37')
+    close_button.click()
+    time.sleep(2)
+    login_button = WebDriverWait(driver, 5).until(
+        EC.element_to_be_clickable((By.CLASS_NAME, '_button_1wo2a_1._login_1wo2a_148')))
+    login_button = driver.find_element(By.CLASS_NAME, '_button_1wo2a_1._login_1wo2a_148')
+    login_button.click()
+    time.sleep(2)
+    email = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="root"]/div[2]/div/div/div[2]/div/div[2]/form/div/label[1]/div[2]/input')))
+    email = driver.find_element(By.XPATH, '//*[@id="root"]/div[2]/div/div/div[2]/div/div[2]/form/div/label[1]/div[2]/input')
+    email.click()
+    email.send_keys(auth_data.email)
+    password = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="root"]/div[2]/div/div/div[2]/div/div[2]/form/div/label[2]/div[2]/input')))
+    password = driver.find_element(By.XPATH, '//*[@id="root"]/div[2]/div/div/div[2]/div/div[2]/form/div/label[2]/div[2]/input')
+    password.click()
+    password.send_keys(auth_data.password)
+    voity = driver.find_element(By.XPATH, '//*[@id="root"]/div[2]/div/div/div[2]/div/div[2]/form/div/button')
+    voity.click()
+    time.sleep(10)
+
+except Exception as ex:
+    print(ex)
+finally:
+    driver.close()
+    driver.quit()
